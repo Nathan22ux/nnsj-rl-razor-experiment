e6657f707d7972aa804297b877553b7c8e781c11
Nathan <nathanallen668@gmail.com>
2025-12-04 02:32:57 -0500

    changed epoch and batch sizes

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ MODEL_NAME = "Qwen/Qwen2.5-3B-Instruct"
 # SFT Hyperparameters (Reference Table 2 located in page 19 on the paper RL razor)
 sft_config = {
     'learning_rates': [3e-5], # [1e-5, 3e-5, 5e-5, 7e-5, 9e-5]
-    'batch_sizes': [16, 32, 64, 128],
-    'epochs': [1, 2],
+    'batch_sizes': [32], # [16, 32, 64, 128]
+    'epochs': [1],
     'lr_scheduler': ['constant_with_warmup', 'cosine_with_warmup'],
     'warmup_steps': 50,
     'optimizer': 'adamw',
