fe746d2906b46cd73fa18769580a4097604033c9
Nathan <nathanallen668@gmail.com>
2025-12-04 01:50:13 -0500

    one elarning rate

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@
 MODEL_NAME = "Qwen/Qwen2.5-3B-Instruct"
 # SFT Hyperparameters (Reference Table 2 located in page 19 on the paper RL razor)
 sft_config = {
-    'learning_rates': [1e-5, 3e-5, 5e-5, 7e-5, 9e-5],
+    'learning_rates': [3e-5], # [1e-5, 3e-5, 5e-5, 7e-5, 9e-5]
     'batch_sizes': [16, 32, 64, 128],
     'epochs': [1, 2],
     'lr_scheduler': ['constant_with_warmup', 'cosine_with_warmup'],
@@ -17,7 +17,7 @@ sft_config = {
 
 # RL Hyperparameters
 rl_config = {
-    'learning_rates': [1e-5, 2e-5, 3e-5, 4e-5, 5e-5],
+    'learning_rates': [2e-5], #[1e-5, 2e-5, 3e-5, 4e-5, 5e-5]
     'epochs': 1,
     'warmup_steps': 50,
     'optimizer': 'adamw',
